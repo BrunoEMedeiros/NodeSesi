0a3027d5cc4447514de475801a0060f6296fff42
D3adSec <bruno.emedeiros99@gmail.com>
2023-10-10 15:30:21 -0300

    aula 10/10

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+//Essa função é responsável por analisar o corpo das solicitações HTTP 
+//que têm um tipo de conteúdo (Content-Type) definido como application/json. 
+//Ela pega os dados JSON enviados na solicitação e os converte em um objeto JS.
+app.use(express.json());
+
+//  Habilita o protocolo CORS para a troca front-back
+//  npm i cors
+app.use(cors());
+
+//Forma comum de codificar dados enviados em solicitações HTTP.
+//Usado quando os dados são enviados de um formulário HTML para uma API. 
+//Nesse formato, os dados são codificados como pares chave-valor, semelhantes aos parâmetros em uma URL.
+app.use(express.urlencoded({
+    extended : true,
+}),
+);
+
+app.get("/",(req, res)=>{
+    let lista = ["bruno","reanto","wagner","ze","alan"]
+    return res.status(200).json(lista);
+});
+
+app.listen(3000,()=>{
+    console.log("api no ar!");
+});
