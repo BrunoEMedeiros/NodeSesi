385e25f84f22ee6dd8355a2588e43c9dd9c1cf90
D3adSec <bruno.emedeiros99@gmail.com>
2023-11-08 11:10:11 -0300

    08/11

--- Produtos.js
+++ Produtos.js
@@ -12,9 +12,3 @@ export class Produto{
     }
 }
 
-export let listaProdutos = [
-    new Produto(1,'tenis',100,2),
-    new Produto(2,'chinelo',20,10),
-    new Produto(3,'detergente',5,30),
-    new Produto(4,'coca-cola',3.75,20),
-]
--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 import express from "express";
 import cors from "cors";
-import { Produtos, listaProdutos } from "./Produtos.js";
+import { Produto } from "./Produtos.js";
+
+let listaProdutos = [
+    new Produto(1,'tenis',100,2),
+    new Produto(2,'chinelo',20,10),
+    new Produto(3,'detergente',5,30),
+    new Produto(4,'coca-cola',3.75,20),
+]
 
 const app = express();
 
@@ -24,19 +31,25 @@ app.get("/produtos",(req, res)=>{
 
 app.post("/produtos/novo",(req,res)=>{
     const { id, desc, preco, qtd } = req.body;
-    listaProdutos.push(new Produtos(id, desc, preco, qtd));
+    listaProdutos.push(new Produto(id, desc, preco, qtd));
     return res.status(200).json("cadastrado com sucesso!");
 })
 
 app.put("/produtos/alterar/:id",(req,res)=>{
     const { id } = req.params;
     const { desc, preco, qtd } = req.body;
-    let produto = listaProdutos.find(obj => obj.id == id)
+    let produto = listaProdutos.find(p => p.id == id);
     produto.desc = desc;
     produto.preco = preco;
     produto.qtd = qtd;
-    return res.status(200).json("alterado com sucesso!");
-})  
+    return res.status(200).json("alterado!");
+});
+
+app.delete("/produtos/excluir/:id",(req,res)=>{
+    let { id } = req.params;
+    listaProdutos = listaProdutos.filter(p => p.id != id)
+    return res.status(200).json("Deletado!");
+});
 
 app.listen(3000,()=>{
     console.log("api no ar!");
