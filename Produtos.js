c360f9aeab4d43ae33806b98470c05a332c82c21
D3adSec <bruno.emedeiros99@gmail.com>
2023-11-08 07:31:01 -0300

    01/11

--- Produtos.js
+++ Produtos.js
@@ -1,10 +1,20 @@
-export class Produtos{
+
+export class Produto{
     id;
-    descricao;
+    desc;
     preco;
-    constructor(id, desc, preco){
+    qtd;
+    constructor(id, desc, preco, qtd){
         this.id = id;
-        this.descricao = desc
-        this.preco = preco
+        this.desc = desc;
+        this.preco = preco;
+        this.qtd = qtd
     }
 }
+
+export let listaProdutos = [
+    new Produto(1,'tenis',100,2),
+    new Produto(2,'chinelo',20,10),
+    new Produto(3,'detergente',5,30),
+    new Produto(4,'coca-cola',3.75,20),
+]
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { Produtos } from "./Produtos.js";
+import { Produtos, listaProdutos } from "./Produtos.js";
 
 const app = express();
 
@@ -16,21 +16,27 @@ app.use(cors());
 //Forma comum de codificar dados enviados em solicitações HTTP.
 //Usado quando os dados são enviados de um formulário HTML para uma API. 
 //Nesse formato, os dados são codificados como pares chave-valor, semelhantes aos parâmetros em uma URL.
-app.use(express.urlencoded({
-    extended : true,
-}),
-);
+app.use(express.urlencoded({ extended : true }));
 
-app.get("/",(req, res)=>{
-    let lista = ["bruno","reanto","wagner","ze","alan"]
-    return res.status(200).json(lista);
-});
-
-app.post("/novo",(req, res)=>{
-    const { id, descricao, preco } = req.body;
-    const produto = new Produtos(id,descricao,preco);
-    return res.status(200).json(produto)
+app.get("/produtos",(req, res)=>{
+    return res.status(200).json(listaProdutos)
 })
+
+app.post("/produtos/novo",(req,res)=>{
+    const { id, desc, preco, qtd } = req.body;
+    listaProdutos.push(new Produtos(id, desc, preco, qtd));
+    return res.status(200).json("cadastrado com sucesso!");
+})
+
+app.put("/produtos/alterar/:id",(req,res)=>{
+    const { id } = req.params;
+    const { desc, preco, qtd } = req.body;
+    let produto = listaProdutos.find(obj => obj.id == id)
+    produto.desc = desc;
+    produto.preco = preco;
+    produto.qtd = qtd;
+    return res.status(200).json("alterado com sucesso!");
+})  
 
 app.listen(3000,()=>{
     console.log("api no ar!");
